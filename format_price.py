face31b15605f4b4d1cfa7fc5c41dad286afe945
appledix <dxl2010@mail.ru>
2017-01-27 19:53:23 +0300

    Initial commit

--- format_price.py
+++ format_price.py
@@ -1,5 +1,34 @@
-def format_price(price):
-    pass
+import argparse
+import re
+
+
+def get_price_from_terminal():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("price", help="Price which you want to format", type=str)
+    args = parser.parse_args()
+    return args.price
+
+def convert_string_to_number(string):
+    string = string.replace(',','.')
+    number = float(string)
+    return (int(number) if number.is_integer() else number)
+
+def format_price(price=None):
+    if isinstance(price, int) and (price >= 0):
+        return "{:,}".format(price).replace(",", " ")
+    elif isinstance(price, float) and (price >= 0):
+        return (format_price(int(price)) if price.is_integer() 
+                else "{:,.2f}".format(price).replace(",", " "))
+    elif isinstance(price, str) and re.match(r'\d+[\.,]?\d*$', price):
+        price = convert_string_to_number(price)
+        return format_price(price)
+    else:
+        raise ValueError
+
+def main():
+    price = get_price_from_terminal()
+    print(format_price(price))
+
 
 if __name__ == '__main__':
-    pass
+    main()
--- tests.py
+++ tests.py
@@ -1,1 +1,76 @@
-# TODO place your tests here
+import unittest
+
+from format_price import format_price
+
+
+class FormatPriceTestCase(unittest.TestCase):
+
+    def test_nothing(self):
+        with self.assertRaises(ValueError): format_price()
+
+    def test_int(self):
+        self.assertEqual('300 000', format_price(300000))
+    
+    def test_int_zero(self):
+        self.assertEqual('0', format_price(0))
+     
+    def test_float(self):
+        self.assertEqual('1 000.35', format_price(1000.35))
+
+    def test_float_int(self):
+        self.assertEqual('1 000', format_price(1000.00))
+
+    def test_float_zero(self):
+        self.assertEqual('0', format_price(0.0))
+
+    def test_float_rounding(self):
+        self.assertEqual('1 000.36', format_price(1000.355923721361263))    
+
+    def test_string_int(self):
+        self.assertEqual('7 668 523', format_price('7668523'))
+
+    def test_string_float_int(self):
+        self.assertEqual('76 685', format_price('76685.00'))
+
+    def test_string_float(self):
+        self.assertEqual('76 685.23', format_price('76685.23'))
+
+    def test_string_float_with_comma(self):
+        self.assertEqual('76 685.23', format_price('76685,23'))
+
+    def test_string_int_zero(self):
+        self.assertEqual('0', format_price('0'))
+
+    def test_string_float_zero(self):
+        self.assertEqual('0', format_price('0.00'))
+
+    def test_string_ends_with_comma(self):
+        self.assertEqual('12 348', format_price('12348,'))
+
+    def test_string_ends_with_dot(self):
+        self.assertEqual('12 348', format_price('12348.'))
+
+    def test_negative_int(self):
+        with self.assertRaises(ValueError): format_price(-100)
+
+    def test_negative_float(self):
+        with self.assertRaises(ValueError): format_price(-50.0)
+
+    def test_negative_string(self):
+        with self.assertRaises(ValueError): format_price('-50.0')
+
+    def test_empty_string(self):
+        with self.assertRaises(ValueError): format_price('')
+
+    def test_string_with_not_numbers(self):
+        with self.assertRaises(ValueError): format_price('31s2abd.e02fg')
+
+    def test_string_with_several_commas(self):
+        with self.assertRaises(ValueError): format_price('12,,,348')
+
+    def test_string_with_several_dots(self):
+        with self.assertRaises(ValueError): format_price('12...348')
+
+
+if __name__ == '__main__':
+    unittest.main()
